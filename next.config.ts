f4f6eb1f9d4fde7a4cf906397d13571c536116a3
shahdmohamednabhan <shahdmohameddbh999@gmail.com>
2026-02-16 21:53:00 +0200

    add

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[{
+      protocol:'https',
+      hostname:"ecommerce.routemisr.com",
+      pathname:"/Route-Academy-*/**"
+
+    }
+    ]
+    
+
+  },
   reactCompiler: true,
 };
 
 export default nextConfig;
+
+  
