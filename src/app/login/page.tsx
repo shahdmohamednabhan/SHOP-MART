26b2933adaf735dfccd0f8a3bb6f7350f3614058
shahdmohamednabhan <shahdmohameddbh999@gmail.com>
2026-02-16 23:01:37 +0200

    fix: make login page dynamic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,29 +1,24 @@
  
- "use client"; // مهم جدًا
-
-import LoginForm from "@/components/Loginform/LoginForm";
-
- export default function Login
- (){
-    return<div className=" flex flex-col gap-3  justify-center items-center h-[60vh]">
-    {/* <h2>Login</h2> */}
-    <LoginForm/>
-    </div>
- }
-
-
-
 //  "use client"; // مهم جدًا
 
 // import LoginForm from "@/components/Loginform/LoginForm";
 
-// export const dynamic = "force-dynamic";
-
-// export default function Login() {
-//   return (
-//     <div className="flex flex-col gap-3 justify-center items-center h-[60vh]">
-//       <LoginForm />
+//  export default function Login
+//  (){
+//     return<div className=" flex flex-col gap-3  justify-center items-center h-[60vh]">
+//     {/* <h2>Login</h2> */}
+//     <LoginForm/>
 //     </div>
-//   );
-// }
+//  }
 
+"use client"; // مهم جدًا
+
+import LoginForm from "@/components/Loginform/LoginForm";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col gap-3 justify-center items-center h-[60vh]">
+      <LoginForm />
+    </div>
+  );
+}
