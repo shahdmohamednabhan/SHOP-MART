8fe36470f2205c06f07d7514ae3733a232f7b461
shahdmohamednabhan <shahdmohameddbh999@gmail.com>
2026-02-16 22:56:32 +0200

    fix: wrap login in suspense

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,31 @@
+git add .
+git commit -m "fix: make login page dynamic"
+git push
+ "use client"; // مهم جدًا
+
+import LoginForm from "@/components/Loginform/LoginForm";
+
+ export default function Login
+ (){
+    return<div className=" flex flex-col gap-3  justify-center items-center h-[60vh]">
+    {/* <h2>Login</h2> */}
+    <LoginForm/>
+    </div>
+ }
+
+
+
+//  "use client"; // مهم جدًا
+
 // import LoginForm from "@/components/Loginform/LoginForm";
 
-//  export default function Login
-//  (){
-//     return<div className=" flex flex-col gap-3  justify-center items-center h-[60vh]">
-//     {/* <h2>Login</h2> */}
-//     <LoginForm/>
+// export const dynamic = "force-dynamic";
+
+// export default function Login() {
+//   return (
+//     <div className="flex flex-col gap-3 justify-center items-center h-[60vh]">
+//       <LoginForm />
 //     </div>
-//  }
+//   );
+// }
 
-
-
-import LoginForm from "@/components/Loginform/LoginForm"
-
-export const dynamic = "force-dynamic"
-
-export default function Login() {
-  return (
-    <div className="flex flex-col gap-3 justify-center items-center h-[60vh]">
-      <LoginForm />
-    </div>
-  )
-}
