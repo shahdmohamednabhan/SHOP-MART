a3a17fbf3d297f8c68593556eb556737217050b9
shahdmohamednabhan <shahdmohameddbh999@gmail.com>
2026-02-16 22:36:58 +0200

    fix: make login page dynamic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,23 @@
-import LoginForm from "@/components/Loginform/LoginForm";
+// import LoginForm from "@/components/Loginform/LoginForm";
 
- export default function Login
- (){
-    return<div className=" flex flex-col gap-3  justify-center items-center h-[60vh]">
-    {/* <h2>Login</h2> */}
-    <LoginForm/>
+//  export default function Login
+//  (){
+//     return<div className=" flex flex-col gap-3  justify-center items-center h-[60vh]">
+//     {/* <h2>Login</h2> */}
+//     <LoginForm/>
+//     </div>
+//  }
+
+
+
+import LoginForm from "@/components/Loginform/LoginForm"
+
+export const dynamic = "force-dynamic"
+
+export default function Login() {
+  return (
+    <div className="flex flex-col gap-3 justify-center items-center h-[60vh]">
+      <LoginForm />
     </div>
- }
+  )
+}
